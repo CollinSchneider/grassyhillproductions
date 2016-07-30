4c59a6eea44e4b3ba957f3b49d1428e38b956fb7
Collin Schneider <collin@jastr.co>
2016-07-30 13:57:09 -0400

    new images

--- app/models/greeting_card.rb
+++ app/models/greeting_card.rb
@@ -1,9 +1,9 @@
 class GreetingCard < ActiveRecord::Base
 
-  has_attached_file :front_image, styles: { large: "600x600!", medium: "300x300!", thumb: "100x100!" }
+  has_attached_file :front_image, styles: { large: "500x700!", medium: "300x300!", thumb: "100x100!" }
   validates_attachment_content_type :front_image, content_type: /\Aimage\/.*\Z/
 
-  has_attached_file :inside_image, styles: { large: "600x600!", medium: "300x300!", thumb: "100x100!" }
+  has_attached_file :inside_image, styles: { large: "500x700!", medium: "300x300!", thumb: "100x100!" }
   validates_attachment_content_type :inside_image, content_type: /\Aimage\/.*\Z/
 
 end
