7db0c6135a53540426ec2a0070837edf982e23e4
Collin Schneider <collin@jastr.co>
2016-09-15 15:42:29 -0400

    removing errors maybe

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    @greeting_card_picture = GreetingCard.all.first
-    @led_picture = LedPicture.all.first
-    @guitar_string_bracelet_picture = GuitarStringBracelet.all.first
+    # @greeting_card_picture = GreetingCard.all.first
+    # @led_picture = LedPicture.all.first
+    # @guitar_string_bracelet_picture = GuitarStringBracelet.all.first
   end
 
   def about
