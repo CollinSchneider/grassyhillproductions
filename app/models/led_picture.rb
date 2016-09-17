6a4c587573b04cbfe5f932a17612eea86e2802c0
Collin Schneider <collin@jastr.co>
2016-09-17 11:25:35 -0400

    fix aws bucket

--- app/models/led_picture.rb
+++ app/models/led_picture.rb
@@ -4,12 +4,12 @@ class LedPicture < ActiveRecord::Base
     styles: { medium: "253x450!"},
     # :default_url => "************",
     # :storage => :s3,
-    :bucket => 'grassyhillproductions'
+    :bucket => 'grassy-hill-productions-1'
     # :s3_credentials => S3_CREDENTIALS (we set this in an initializer)
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   has_attached_file :gif,
     styles: { medium: "253x450!" },
-    :bucket => 'grassyhillproductions'
+    :bucket => 'grassy-hill-productions-1'
 
 end
