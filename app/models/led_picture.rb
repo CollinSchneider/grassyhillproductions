9f76f8abd3f1dec5968ca83d9adaa3f01cabf1ba
Collin Schneider <collin@jastr.co>
2017-07-25 19:47:20 -0400

    new workflow for updating with new images

--- app/models/led_picture.rb
+++ app/models/led_picture.rb
@@ -12,4 +12,32 @@ class LedPicture < ActiveRecord::Base
     styles: { medium: "850x475!" },
     :bucket => 'grassy-hill-productions-1'
 
+  def self.upload_files_job
+    already_created = []
+    Dir.foreach("#{Rails.root}/leds") do |dir|
+      next if dir == '.' || dir == '..'
+      files = Dir["#{Rails.root}/leds/#{dir}/*"]
+      dir_names = dir.split('..')
+      code = dir_names[0]
+      title = dir_names[1]
+      led_exists = LedPicture.find_by(code: code)
+      if led_exists
+        already_created << "#{code} already exists, cannot add. "
+      else
+        puts "Adding #{title}....."
+        led = LedPicture.create(code: code, title: title)
+        files.each do |file|
+          if file.include?('gif')
+            led.gif = File.open(file, 'r')
+          else
+            led.image = File.open(file, 'r')
+          end
+        end
+        led.save!
+      end
+    end
+    return already_created.join(',') if already_created.any?
+    return 'Upload Successful!'
+  end
+
 end
